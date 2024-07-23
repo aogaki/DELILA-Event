043d3f9519ee8ce16c7f48fc0602d7127db02b22
Soiciro <sohichiroh.aogaki@eli-np.ro>
2024-07-23 12:45:51 +0300

    Adding making dictionar makefile

--- dictionary/LinkDef.h
+++ dictionary/LinkDef.h
@@ -2,5 +2,7 @@
 
 #pragma link C++ class THitData + ;
 #pragma link C++ class std::vector < THitData> + ;
+#pragma link C++ class HitData_t + ;
+#pragma link C++ class std::vector < HitData_t> + ;
 
 #endif
